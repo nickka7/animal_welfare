49f48970b22db2705f86a1f53f10edd193b68017
moonpoppap <poppapzaako@hotmail.com>
2022-03-21 12:58:21 +0700

    downloadFile page test session-timeout success

--- lib/screens/document/DownloadFile.dart
+++ lib/screens/document/DownloadFile.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:animal_welfare/screens/setting/setting_logout.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -24,9 +25,12 @@ class _DownloadFileState extends State<DownloadFile> {
     String? token = await storage.read(key: 'token');
     var response = await http.get(Uri.parse('$endPoint/api/getDocument'),
         headers: {"authorization": 'Bearer $token'});
-    print(response.body);
+    // print(response.body);
+    // var z = jsonDecode(response.body)['errorMessage'];
+    // print(z);
+    // var y = json.decode(response.body)['errorMessage'];
+    // print(y);
     var jsonData = Document.fromJson(jsonDecode(response.body));
-    print(jsonData);
     return jsonData;
   }
 
@@ -43,6 +47,9 @@ class _DownloadFileState extends State<DownloadFile> {
             future: getDocument(),
             builder: (BuildContext context, AsyncSnapshot<Document> snapshot) {
               if (snapshot.hasData) {
+                if (snapshot.data!.errorMessage == "session-expired") {
+                  setState((){UserLogout().clearTokenAndLogout(context);});
+                }
                 return ListView.builder(
                   shrinkWrap: true,
                   scrollDirection: Axis.vertical,
@@ -51,12 +58,13 @@ class _DownloadFileState extends State<DownloadFile> {
                     return Card(
                       child: ListTile(
                         leading: Icon(Icons.document_scanner_outlined),
-                        title: Text('${snapshot.data!.data![index].documentName}'),
+                        title:
+                            Text('${snapshot.data!.data![index].documentName}'),
                         onTap: () => openFile(
+                          url: '${snapshot.data!.data![index].url}',
                           // '', url: 'https://www.ocsc.go.th/sites/default/files/document/example_calculation25552.xls',
                           //    url: 'http://tls.labour.go.th/attachments/category/118/0000001%20tls%2003%202563.doc',
                           // https://shortrecap.co/wp-content/uploads/2020/05/Catcover_web.jpg
-                          url: '${snapshot.data!.data![index].url}',
                           //  '',
                           // url:'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_2mb.mp4'
                           //   'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_2mb.mp4',
@@ -89,7 +97,7 @@ class _DownloadFileState extends State<DownloadFile> {
     final name = fileName ?? url.split('/').last;
     final file = await downloadFile(url, name);
     if (file == null) return;
-    print('Path : ${file.path}');
+    // print('Path : ${file.path}');
     OpenFile.open(file.path);
   }
 
